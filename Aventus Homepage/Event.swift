9b720c3ba751f7738ef4f2a8516c729053bad1c6
Weixiong Tay <weixiongtay@Weixiongs-Mac.local>
2018-02-19 22:09:31 +0000

    Modified Event class to include Seat class

--- Aventus Homepage/Event.swift
+++ Aventus Homepage/Event.swift
@@ -26,7 +26,7 @@ class Event{
     
     // MARK: Initialization
     init?(json: [String: Any]) {
-        guard let jsonartist = json["Artist"] as? String else{
+        guard let jsonartist = json["Fake Mainstream Events"] as? String else{
             return nil
         }
         self.artist = jsonartist
@@ -43,7 +43,41 @@ class Event{
         self.datetime = jsondatetime
         self.description = nil
         self.photo = nil
-        self.seating = Seating(categories: ["cat A", "cat B"], price: [50, 150], noSeatsAvail: [100, 100], noCategories: 2)
+        //creating Seat class
+        var jsonPrice = [Int]()
+        
+        guard let jsoncata = json["Category 1 Price"] as? String else{
+            print("price error 1")
+            return nil
+        }
+        let cata = Int(jsoncata)
+        guard let jsoncatb = json["Category 2: Price"] as? String else{
+            print("price error 2")
+            return nil
+        }
+        let catb = Int(jsoncatb)
+        guard let jsoncatc = json["Category 3: Price"] as? String else{
+            print("price error 3")
+            return nil
+        }
+        let catc = Int(jsoncatc)
+        guard let jsoncatd = json["Category 4: Price"] as? String else{
+            print("price error 4")
+            return nil
+        }
+        let catd = Int(jsoncatd)
+        guard let jsoncate = json["Category 5(standing area): Price"] as? String else{
+            print("price error 5")
+            return nil
+        }
+        let cate = Int(jsoncate)
+        jsonPrice += [cata!, catb!, catc!, catd!, cate!]
+        
+        let seats = Seating(categories: ["CatA", "CatB", "CatC", "CatD", "Standing"], price: jsonPrice, noSeatsAvail: [10,10,0,10,10], noCategories: jsonPrice.count)
+        
+        
+        self.seating = seats
+        
     }
     
     
