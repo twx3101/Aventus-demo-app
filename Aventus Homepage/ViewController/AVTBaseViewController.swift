23256c9a05eeb9de06f333e86c81309cd0c676bc
Krongsiriwat <kk2017@cider01.doc.ic.ac.uk>
2018-05-14 23:11:40 +0100

    change icon, comment

--- Aventus Homepage/ViewController/AVTBaseViewController.swift
+++ Aventus Homepage/ViewController/AVTBaseViewController.swift
@@ -18,7 +18,7 @@ class AVTBaseViewController: UIViewController, UITextFieldDelegate {
     var ticketButton: UIButton = UIButton(type: UIButtonType.system)
     
     var menuButton: UIButton = UIButton(type: UIButtonType.system)
-    var searchButton: UIButton = UIButton(type: UIButtonType.system)
+    var chatButton: UIButton = UIButton(type: UIButtonType.system)
 
     var textControl: UITextField = SearchTextField(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
     var cancelButton: UIButton = UIButton(type: UIButtonType.system)
@@ -59,39 +59,44 @@ class AVTBaseViewController: UIViewController, UITextFieldDelegate {
         
         textfieldWidth = Int(self.view.frame.width) - (sideMargin*2) - buttonSize
         
+        // The first view at the top of the page consists of menu button and chat button
+        // This view appears when the page is first loaded
+        
         menuButton.frame = CGRect(x: leftItemX + (buttonSize - menuButtonSize)/2, y: topMargin + (buttonSize - menuButtonSize)/2, width: menuButtonSize, height: menuButtonSize)
-        //menuButton.addTarget(self, action: #selector(openMenu), for:    .touchUpInside)
         menuButton.setImage(UIImage(named: "menu"), for: UIControlState())
     
         menuButton.tintColor = .white
         
         view.addSubview(menuButton)
         
-        searchButton.frame = CGRect(x: rightItemX, y: topMargin, width: buttonSize, height: buttonSize)
-        searchButton.addTarget(self, action: #selector(showSearchBarView), for:    .touchUpInside)
-        searchButton.setImage(UIImage(named: "search"), for: UIControlState())
-        searchButton.tintColor = .white
+        chatButton.frame = CGRect(x: rightItemX, y: topMargin, width: buttonSize, height: buttonSize)
+        chatButton.addTarget(self, action: #selector(showSearchBarView), for: .touchUpInside)
+        chatButton.setImage(UIImage(named: "speechIcon"), for: UIControlState())
+        chatButton.tintColor = .white
         
         view.addSubview(menuButton)
-        view.addSubview(searchButton)
+        view.addSubview(chatButton)
         
-        
+        // The second view at the top of the page consists of the search bar (waiting for user input) and cancel button (to exit editing mode)
+        // This view appears when users click the chat button in the first view.
         
         textControl.frame = CGRect(x: leftItemX, y: topMargin, width: textfieldWidth, height: textfieldHeight)
-        
         textControl.delegate = self
         
         cancelButton.frame = CGRect(x: rightItemX, y: topMargin, width: buttonSize, height: buttonSize)
-        cancelButton.addTarget(self, action: #selector(hideSearchBarView), for:    .touchUpInside)
+        cancelButton.addTarget(self, action: #selector(hideSearchBarView), for: .touchUpInside)
         cancelButton.setImage(UIImage(named: "cancel"), for: UIControlState())
         cancelButton.tintColor = .white
         
         view.addSubview(cancelButton)
         view.addSubview(textControl)
         
+        
+        // The sidebar consists of help button and ticket button.
+        // Users can navigate to help page and ticket page by clicking the button.
+        
         menuView.frame = CGRect(x: 0, y: 0 , width: menuWidth, height: Int(self.view.frame.size.height))
         menuView.backgroundColor = colors.menuBar
-        
         
         helpButton.frame = CGRect(x: sideMargin/5, y: (Int(self.view.frame.height/2) - 50) , width: menuWidth, height: buttonSize)
         helpButton.addTarget(self, action: #selector(navHelp), for:    .touchUpInside)
@@ -99,40 +104,34 @@ class AVTBaseViewController: UIViewController, UITextFieldDelegate {
         helpButton.setImage(UIImage(named: "helpIcon"), for: UIControlState())
         helpButton.setTitle("  Help", for: UIControlState())
         
-        
         ticketButton.frame = CGRect(x: sideMargin/5, y: (Int(self.view.frame.height/2) + 50), width: menuWidth, height: buttonSize)
         ticketButton.tintColor = .white
-        ticketButton.addTarget(self, action: #selector(navTicket), for:    .touchUpInside)
+        ticketButton.addTarget(self, action: #selector(navTicket), for: .touchUpInside)
         ticketButton.setImage(UIImage(named: "ticketIcon"), for: UIControlState())
         ticketButton.setTitle("  Ticket", for: UIControlState())
-        
         
         view.addSubview(menuView)
         view.addSubview(helpButton)
         view.addSubview(ticketButton)
         
+        // Microphone button
         
         micButton.center.x = self.view.center.x
         micButton.center.y = self.view.frame.maxY - 30
-        micButton.addTarget(self, action: #selector(micPress), for:    .touchUpInside)
+        micButton.addTarget(self, action: #selector(micPress), for: .touchUpInside)
         micButton.setImage(UIImage(named: "icons8-microphone-96"), for: UIControlState())
         
         view.addSubview(micButton)
-        
-        //micButton.isEnabled = true
-        
-        
+
         hideMenuBase()
         hideSearchBarView()
         
-        
     }
     
+    // When microphone button is pressed, the application proceeds the input given by users.
     func micPress() {
         // let startSound: SystemSoundID = 1110
         //let endSound : SystemSoundID = 1111
-        
-        print("pressed")
         
         if self.isRecording {
             // AudioServicesPlaySystemSound(endSound)
@@ -147,76 +146,66 @@ class AVTBaseViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    // Navigate users to Ticket page
     func navTicket() {
-        
         hideMenuBase()
-
         let detailViewController = self.storyboard?.instantiateViewController(withIdentifier: "TicketPage") as! TicketViewController
-        
         present(detailViewController, animated: true, completion: nil)
-    
     }
     
+    // Navigate users to Help page
     func navHelp() {
         hideMenuBase()
         let detailViewController = self.storyboard?.instantiateViewController(withIdentifier: "HelpPage") as! HelpViewController
-        
         let pageViewController = self.parent as! PageViewController
-        
         detailViewController.pageIndex = pageViewController.pages.index(of: self)
-        
         present(detailViewController, animated: true, completion: nil)
     }
     
+    // Show menu side bar
     func showMenuBase() {
-        
-        menuView.isHidden = false
-        ticketButton.isHidden = false
-        helpButton.isHidden = false
+        UIView.animate(withDuration: 1, animations: {
+            self.menuView.isHidden = false
+            self.ticketButton.isHidden = false
+            self.helpButton.isHidden = false
+        }, completion: nil)
     }
     
+    // Hide menu side bar
     func hideMenuBase() {
-        ticketButton.isHidden = true
-        helpButton.isHidden = true
-        menuView.isHidden = true
-        
+        UIView.animate(withDuration: 1, animations: {
+            self.ticketButton.isHidden = true
+            self.helpButton.isHidden = true
+            self.menuView.isHidden = true
+        }, completion: nil)
     }
     
+    // Show search bar
     func showSearchBarView() {
-
-        
         UIView.animate(withDuration: 0.5, animations: {
             self.cancelButton.isHidden = false
             self.textControl.isHidden = false
-
             self.menuButton.isHidden = true
-            self.searchButton.isHidden = true
+            self.chatButton.isHidden = true
         }, completion: { finished in self.textControl.becomeFirstResponder()})
     }
     
-    
-    func openMenu() {
-    }
-    
+    // Hide search bar
     func hideSearchBarView() {
-
-        
         UIView.animate(withDuration: 0.5, animations: {
             self.cancelButton.isHidden = true
             self.textControl.isHidden = true
             
             self.menuButton.isHidden = false
-            self.searchButton.isHidden = false
+            self.chatButton.isHidden = false
         }, completion: nil)
         view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         if let text = self.textControl.text{
